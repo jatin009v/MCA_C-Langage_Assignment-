79d8eeb7d5053868e2c0e7faff1db2507d576adf
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2025-08-13 11:47:25 +0530

    Update check if a given number is a palindrome.c

--- Assigment-2/check if a given number is a palindrome.c
+++ Assigment-2/check if a given number is a palindrome.c
@@ -1,2 +1,32 @@
 //10. Write a C program that implements a program to check if a given
+
 // number is a palindrome using a while loop.
+
+#include <stdio.h>
+
+int main() {
+    int num, original, reversed = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    if (original == reversed)
+        printf("Palindrome number.\n");
+    else
+        printf("Not a palindrome number.\n");
+
+    return 0;
+}
+
+
+//output
+//Enter a number: 121
+//Palindrome number.
