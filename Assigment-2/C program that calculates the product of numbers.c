8025506cbc240e569ad96eff43132434063643c8
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2025-08-13 11:44:18 +0530

    Update C program that calculates the product of numbers.c

--- Assigment-2/C program that calculates the product of numbers.c
+++ Assigment-2/C program that calculates the product of numbers.c
@@ -1,1 +1,20 @@
 //8. Write a C program that calculates the product of numbers from 1 to 5 using a while loop.
+
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    long product = 1;
+
+    while (i <= 5) {
+        product *= i;
+        i++;
+    }
+
+    printf("Product of numbers from 1 to 5: %ld\n", product);
+    return 0;
+}
+
+
+//Output:
+//Product of numbers from 1 to 5: 120
