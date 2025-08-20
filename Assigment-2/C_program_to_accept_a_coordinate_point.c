ca636402fec1525269daead8ff960598e4b19cb1
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2025-08-20 08:03:10 +0530

    Update C_program_to_accept_a_coordinate_point.c

--- Assigment-2/C_program_to_accept_a_coordinate_point.c
+++ Assigment-2/C_program_to_accept_a_coordinate_point.c
@@ -1,2 +1,35 @@
 //2. Write a C program to accept a coordinate point in an XY coordinate system
 //and determine in which quadrant the coordinate point lies.
+
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    printf("Enter X coordinate: ");
+    scanf("%d", &x);
+    printf("Enter Y coordinate: ");
+    scanf("%d", &y);
+
+    if (x > 0 && y > 0)
+        printf("Point lies in Quadrant 1\n");
+    else if (x < 0 && y > 0)
+        printf("Point lies in Quadrant 2\n");
+    else if (x < 0 && y < 0)
+        printf("Point lies in Quadrant 3\n");
+    else if (x > 0 && y < 0)
+        printf("Point lies in Quadrant 4\n");
+    else if (x == 0 && y == 0)
+        printf("Point is at the Origin\n");
+    else if (x == 0)
+        printf("Point lies on Y-axis\n");
+    else
+        printf("Point lies on X-axis\n");
+
+    return 0;
+}
+
+
+//output
+//Enter X coordinate: -4
+//Enter Y coordinate: 7
+//Point lies in Quadrant 2
