ddf6226951d97060744fe6fed5c0e1d25607419b
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2025-08-20 08:05:18 +0530

    Update Fibonacci Sequence Generation.c

--- Assigment-2/Fibonacci Sequence Generation.c
+++ Assigment-2/Fibonacci Sequence Generation.c
@@ -1,1 +1,24 @@
 //11. Fibonacci Sequence Generation Using a While Loop.
+#include <stdio.h>
+
+int main() {
+    int n, first = 0, second = 1, next, i = 1;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    while (i <= n) {
+        printf("%d ", first);
+        next = first + second;
+        first = second;
+        second = next;
+        i++;
+    }
+
+    printf("\n");
+    return 0;
+}
+
+//output
+//Enter number of terms: 7
+//0 1 1 2 3 5 8
