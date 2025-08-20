13dfeab4edd15f38a053601e252cd5189b45fa26
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2025-08-20 07:58:36 +0530

    Update C program that prompts the user to enter a positive integer.c

--- Assigment-2/C program that prompts the user to enter a positive integer.c
+++ Assigment-2/C program that prompts the user to enter a positive integer.c
@@ -1,1 +1,25 @@
-//12. Write a C program that prompts the user to enter a positive integer.
+//12. Write a C program that prompts the user to enter a positive integer. 
+//Use a while loop to print the multiplication table for that number up to 10.
+
+#include <stdio.h>
+
+int main() {
+    int num, i = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    while (i <= 10) {
+        printf("%d x %d = %d\n", num, i, num * i);
+        i++;
+    }
+
+    return 0;
+}
+
+//Enter a positive integer: 5
+//5 x 1 = 5
+//5 x 2 = 10
+//5 x 3 = 15
+//...
+//5 x 10 = 50
