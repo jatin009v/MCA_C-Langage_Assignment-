ecd30b50dcafd3eb3633bc2ad2f0cd041ba7b85f
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2025-08-20 08:04:12 +0530

    Update C_program_to_check_whether_a_triangle_is_Equilateral.c

--- Assigment-2/C_program_to_check_whether_a_triangle_is_Equilateral.c
+++ Assigment-2/C_program_to_check_whether_a_triangle_is_Equilateral.c
@@ -1,1 +1,22 @@
 //3. Write a C program to check whether a triangle is Equilateral, Isosceles or Scalene.
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    printf("Enter three sides of triangle: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c)
+        printf("Equilateral Triangle\n");
+    else if (a == b || b == c || a == c)
+        printf("Isosceles Triangle\n");
+    else
+        printf("Scalene Triangle\n");
+
+    return 0;
+}
+
+
+//output
+//Enter three sides of triangle: 5 5 8
+//Isosceles Triangle
