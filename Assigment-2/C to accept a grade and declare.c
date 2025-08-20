b0f207f7ede75cf0d21ab0b1bc20bb5a6d92a382
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2025-08-20 08:02:10 +0530

    Update C to accept a grade and declare.c

--- Assigment-2/C to accept a grade and declare.c
+++ Assigment-2/C to accept a grade and declare.c
@@ -11,3 +11,27 @@
 
 // F Fail
 
+#include <stdio.h>
+
+int main() {
+    char grade;
+    printf("Enter grade (E/V/G/A/F): ");
+    scanf(" %c", &grade);
+
+    switch (grade) {
+        case 'E': printf("Excellent\n"); break;
+        case 'V': printf("Very Good\n"); break;
+        case 'G': printf("Good\n"); break;
+        case 'A': printf("Average\n"); break;
+        case 'F': printf("Fail\n"); break;
+        default: printf("Invalid grade\n");
+    }
+
+    return 0;
+}
+
+
+//output
+///Enter grade (E/V/G/A/F): G
+//Good
+
