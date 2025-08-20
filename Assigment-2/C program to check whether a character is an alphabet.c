35391bed8db15a2a3cd1d959b1819f83b59e8d4d
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2025-08-20 07:59:52 +0530

    Update C program to check whether a character is an alphabet.c

--- Assigment-2/C program to check whether a character is an alphabet.c
+++ Assigment-2/C program to check whether a character is an alphabet.c
@@ -1,1 +1,25 @@
-//4. Write a C program to check whether a character is an alphabet, digit or special character.
+//4. 4. Write a C program to check whether a character is an alphabet, digit or
+// special character.
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if (isalpha(ch))
+        printf("Alphabet\n");
+    else if (isdigit(ch))
+        printf("Digit\n");
+    else
+        printf("Special Character\n");
+
+    return 0;
+}
+
+
+//output
+//Enter a character: @
+//Special Character
